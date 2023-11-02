1efeb02976eff83cf26e61dcbd3c791cfed408db
Mustafa Cakir <144024304+MxCann@users.noreply.github.com>
2023-11-02 14:40:19 +0300

    Initial Commit

---
+++ UIActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIActivityIndicator
+//
+//  Created by Mustafa Çakır on 2.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
